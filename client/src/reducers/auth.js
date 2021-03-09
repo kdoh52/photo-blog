70f31bb702c7ed02efc5e42265b8a25cbf0ba1ed
kdoh52 <kdoh52@gmail.com>
2021-03-09 15:27:31 -0800

    google oauth logout

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -10,8 +10,10 @@ const authReducer = (state = { authData: null }, action) => {
             localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
             // change authData to equal same thing
             return { ...state, authData: action?.data }
-        // case LOGOUT:
-        //     return [ ...state, action.payload ];
+        case LOGOUT:
+            // clear local storage and set authData to null
+            localStorage.clear();
+            return { ...state, authData: null }
         default:
             return state;
     }
